617fccd7405f99e2d770b882e184f22a4ccfaeca
Nikhil <nikhilbchandran@gmail.com>
2025-12-14 22:20:51 +0530

    ready for host

--- server.js
+++ server.js
@@ -12,4 +12,7 @@ app.use(express.json());
 app.use("/api/quiz", quizRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Backend is live 🚀" });
+});
